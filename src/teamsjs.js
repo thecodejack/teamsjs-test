c5ee4e184bb5f1159ad877101634f090a9bc2aef
thecodejack <thecodejack@users.noreply.github.com>
2024-11-09 22:54:48 +0530

    Update teamsjs.js

--- src/teamsjs.js
+++ src/teamsjs.js
@@ -1,17 +1,36 @@
-import { teamsCore, app } from "@microsoft/teams-js";
+import { teamsCore, app, sendCustomMessage as sendCustomMessageMetaOS } from "@microsoft/teams-js";
 import { initializeMetaOsAppSdk } from "./initializeMetaOSSDK";
+
 
 export async function registerTeamsJSHooks() {
   await initializeMetaOsAppSdk();
   teamsCore.registerOnLoadHandler(async (loadContext) => {
-    app.notifyAppLoaded();
-    app.notifySuccess();
+    if(isAppCachingEnabled()) {
+      notifyReadyToUnload();
+    } else {
+      app.notifyAppLoaded();
+      app.notifySuccess(); 
+    }
   });
 
   teamsCore.registerBeforeUnloadHandler((readyToUnload) => {
-    readyToUnload();
+    // Add the query string to the current URL
+    const currentUrl = new URL(window.location.href);
+    currentUrl.searchParams.set('isAppCaching', 'true');
+    window.history.replaceState({}, '', currentUrl.toString());
     return true;
   });
+}
+
+export function function isAppCachingEnabled() {
+  const currentUrl = new URL(window.location.href);
+  return currentUrl.searchParams.get('isAppCaching') === 'true';
+}
+
+export function notifyReadyToUnload() {
+    setTimeout(() => {
+        sendCustomMessageMetaOS('readyToUnload');
+    }, 10000); // 10 seconds (10000 milliseconds)
 }
 
 registerTeamsJSHooks();
